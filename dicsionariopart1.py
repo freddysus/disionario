93911a65d14ac85241c43d13821ea98be30f3844
freddysus <freddysus821@gmail.com>
2024-08-31 15:15:48 -0500

    Create dicsionariopart1.py

---
+++ dicsionariopart1.py
@@ -0,0 +1,12 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "funar" : "una funa es una epudio público contra una persona "
+            "sus": "esto representa que algo se te ase sespechoso su origen es en el juego among us"
+            "wey" : "es una forma de dirigirse a un amigo" }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+   # ¿Qué debemos hacer si se encuentra la palabra?
+else: print (perdon no tenemos esa asa palabra)
+   #¿Qué hacer si no se encuentra la palabra?
